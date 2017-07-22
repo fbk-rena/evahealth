d4affac71bb722facdb1b0175ad16c723a788941
fbk-rena <fbk.rena@gmail.com>
2017-07-22 16:16:08 -0500

    landing colores definidos

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,25 +1,26 @@
- window.fbAsyncInit = function () {
-     FB.init({
-         appId: '1693902830919104',
-         autoLogAppEvents: true,
-         xfbml: true,
-         version: 'v2.10'
-     });
-     FB.AppEvents.logPageView();
- };
-
- (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.10&appId=1693902830919104";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-FB.getLoginStatus(function(response) {
+$(document).ready(
+    FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
+        console.log(statusChangeCallback(response));
 });
+)
+ window.fbAsyncInit = function() {
+            FB.init({
+                appId: '1693902830919104',
+                cookie: true,
+                xfbml: true,
+                version: 'v2.8'
+            });
+            FB.AppEvents.logPageView();
+        };
 
- FB.login()
- FB.getAuthResponse()
-
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
